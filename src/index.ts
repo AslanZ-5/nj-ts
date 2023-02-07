2677327be335822f83ce018ae9f2c26ff252442d
Aslan <asl.zurabov@gmail.com>
2023-02-07 16:00:58 +0300

    ts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+class Invoce {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d:string, a:number){
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+
+    format(){
+        return `${this.client} owes ${this.amount} for ${this.details}`
+    }
+}
+
+const invOne = new Invoce("asasd", "dkdhg wed", 3232)
+const invTwo = new Invoce("sd", "dkdhg", 32)
+
+console.log(invOne, invTwo)
